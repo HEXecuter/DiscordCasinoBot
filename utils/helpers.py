8a3875994b36659fa7df84a384ea543aed5c0260
BuildTools <ealvarez2025@gmail.com>
2023-03-13 16:16:04 -0700

    Add helper method for human readable timedelta

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,6 +7,7 @@ from models.model import User
 from models.model import Multipliers
 from decimal import Decimal
 from locale import currency
+from datetime import timedelta
 
 
 def get_user(session: Session, discord_id: int, guild_id: int) -> Union[None | User]:
@@ -26,6 +27,36 @@ def get_multipliers(user: User) -> Union[Decimal]:
 
 def format_money(amount: Union[Decimal | int | float]) -> str:
     return currency(amount, grouping=True)
+
+
+def format_timedelta(time: timedelta) -> str:
+    seconds_in_hour: int = 3600
+    seconds_in_minute: int = 60
+
+    time_list = []
+    remaining_seconds: int = int(time.total_seconds())
+    if remaining_seconds >= seconds_in_hour:
+        remaining_hours = remaining_seconds // seconds_in_hour
+        time_list.append(f'{remaining_hours} hour' + ('s' if remaining_hours == 1 else ''))
+        remaining_seconds %= seconds_in_hour
+
+    if remaining_seconds >= seconds_in_minute:
+        remaining_minutes = remaining_seconds // seconds_in_minute
+        time_list.append(f'{remaining_minutes} minute' + ('s' if remaining_minutes == 1 else ''))
+        remaining_seconds %= seconds_in_minute
+
+    if remaining_seconds:
+        time_list.append(f'{remaining_seconds} second' + ('s' if remaining_seconds == 1 else ''))
+
+    if len(time_list) > 1:
+        time_string: str = ', '.join(time_list[0:-1])
+        time_string += ', and ' + time_list[-1]
+    elif len(time_list) == 0:
+        time_string = '0 seconds'
+    else:
+        time_string = time_list[0]
+
+    return time_string
 
 
 async def send_response(interaction: nextcord.Interaction, **kwargs):
